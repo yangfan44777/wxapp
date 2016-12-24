2cf1ac0bc14d48459f262e4e34e2a5999bd055f9
yang.fan44777@hotmail.com <yang.fan44777@hotmail.com>
2016-12-24 16:55:53 +0800

    fix

--- src/routes/paycb.js
+++ src/routes/paycb.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var request = require('request');
+var wxPayment = require('wx-payment');
 
 
 /*router.post('/', function(req, res, next) {
