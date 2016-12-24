4562955cb636f2c75c240945e78ec563e0e9f6c7
yang.fan44777@hotmail.com <yang.fan44777@hotmail.com>
2016-12-24 16:07:04 +0800

    fix

--- src/routes/payorder.js
+++ src/routes/payorder.js
@@ -41,10 +41,10 @@ router.post('/', function(req, res, next) {
 
 	      	wxPayment.getBrandWCPayRequestParams({
 			  	body: '支付测试', // 商品或支付单简要描述
-			  	out_trade_no: 'order1', // 商户系统内部的订单号,32个字符内、可包含字母
+			  	out_trade_no: 'orderabc', // 商户系统内部的订单号,32个字符内、可包含字母
 			  	total_fee: 100, //订单总金额，单位为分
 			  	spbill_create_ip: remoteAddress, //APP和网页支付提交用户端ip，Native支付填调用微信支付API的机器IP
-			  	notify_url: 'https://petadore.cn/paycb',
+			  	notify_url: encodeURIComponent('https://petadore.cn/paycb'),
 			  	trade_type: 'JSAPI',
 			  	product_id: '1234567890',
 			  	openid: openid
