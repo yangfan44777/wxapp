d926033873fbb355ee54d21cc8a78484d8f69158
yang.fan44777@hotmail.com <yang.fan44777@hotmail.com>
2016-12-24 16:54:22 +0800

    fix

--- src/routes/paycb.js
+++ src/routes/paycb.js
@@ -3,9 +3,16 @@ var router = express.Router();
 var request = require('request');
 
 
-router.post('/', function(req, res, next) {
+/*router.post('/', function(req, res, next) {
 	console.log(req.query);
 	return res.json(req.query);
-});
+});*/
+
+router.post('/', wxPayment.wxCallback(function(msg, req, res, next){
+  // 处理商户业务逻辑
+  console.log(msg);
+  // res.success() 向微信返回处理成功信息，res.fail()返回失败信息。
+  res.success();
+}));
 
 module.exports = router;
